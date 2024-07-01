d9347fec7b7d8c85732be7e19c3dd48b3cc342fe
anon-snus <148210808+anon-snus@users.noreply.github.com>
2024-07-01 21:54:08 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,44 @@
+max_gwei=30
+
+from_sleep=150
+to_sleep=300
+
+linea_rpc= 'https://linea.decubate.com'
+eth_rpc='https://ethereum-rpc.publicnode.com'
+
+
+CEX='okx' #binance, okx, mexc, kucoin, gate, huobi, bybit, bitget
+proxy_server='http://l11CQd:ryathpo@47.9.55.230:3000'
+amount=[0.0003,0.0003]
+delay=[10, 20]
+# api keys
+
+
+class api:
+	# binance API
+	binance_apikey = 'your_api'
+	binance_apisecret = 'your_api_secret'
+	# okx API
+	okx_apikey = "your_apikey"
+	okx_apisecret = "your_apisecret"
+	okx_passphrase = "your_pass"
+	# bitget API
+	bitget_apikey = "your_api"
+	bitget_apisecret = "your_api_secret"
+	bitget_passphrase = "your_passphrase"
+	# bybit API
+	bybit_apikey = "your_api"
+	bybit_apisecret = "your_api_secret"
+	# gate API
+	gate_apikey = "your_api"
+	gate_apisecret = "your_api_secret"
+	# kucoin API
+	kucoin_apikey = "your_api"
+	kucoin_apisecret = "your_api_secret"
+	kucoin_passphrase = "your_api_password"
+	# mexc API
+	mexc_apikey = "your_api"
+	mexc_apisecret = "your_api_secret"
+	# huobi API
+	huobi_apikey = "your_api"
+	huobi_apisecret = "your_api_secret"
